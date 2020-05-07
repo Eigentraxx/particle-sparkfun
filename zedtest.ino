a3d2f4322ebc15ef3b9b1e2c1e94265b61fea222
Ralph Grathoff <44266333+Eigentraxx@users.noreply.github.com>
2020-05-07 05:23:08 -0500

    Update zedtest.ino

--- zedtest.ino
+++ zedtest.ino
@@ -2,57 +2,78 @@
 #include <ArduinoJson.h>
 
 // This #include statement was automatically added by the Particle IDE.
-#include "gps.h"
+
 #include "SparkFun_Ublox_Arduino_Library.h"
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
+#include "gps.h"
 
 long lastTime = 0; //Simple local timer.
 int httpCmd(String command);
+int httpdbSet(String cmd); // change database location, i.e. firebase reference
 
 int httpStat = 0;
 
-void setup()
-{
+char dbLoc[100] = "default"; // always default to start
+char * message = "eigen_one"; // particle boron identifier corresponding to firebase db
+
+void setup() {
   Serial.begin(9600);
-    guidFunc();
-    setupGpsModule();
-    Particle.function("httpCmd", httpCmd);
+  guidFunc();
+  setupGpsModule();
+  Particle.function("httpCmd", httpCmd);
+  Particle.function("httpdbSet", httpdbSet);
+  Particle.variable("id", message, STRING);
+
 }
 
-void loop()
-{
-  // every 30 seconds for testing
-  if (millis() - lastTime > 60000)
-  {
+void loop() {
+  // every 60 seconds for testing
+  if (millis() - lastTime > 60000) {
     lastTime = millis(); //Update the timer
-    getGpsData();
-    if (httpStat == 1){    //  poll stat end when true
-       while(1) { } // end program
+    getGpsData(dbLoc);
+    if (httpStat == 1) { //  poll stat end when true
+      while (1) {} // end program
     }
   }
+
 }
-http function
+
 /**
- * call back on particle function
- * @param  command - string from http ; currently 'callgps' & 'off'
- * @return     returns an integer 1 === off 
+ * [httpdbSet description]: set database location from http request
+ * @param  cmd [description] : new string from http used by web hook when data is sent to firebase
+ * @return integer  [description] : just any number for now.
  */
- int httpCmd(String command)
-{
-  // look for the matching argument "coffee" <-- max of 64 characters long
-  if(command == "off")
-  {
-      httpStat = 1;
+int httpdbSet(String cmd) {
+  strcpy(dbLoc, cmd);
+  // dbLoc = cmd;
+  return 4;
+
+}
+
+/**
+ * [httpCmd description] : string from http request
+ * @param  command [description]: request String off sleep gps
+ * @return integer      [description] integer still need to assign to webapp
+ */
+int httpCmd(String command) {
+
+  if (command == "off") {
+    httpStat = 1;
     return 1;
   }
-   if(command == "callgps")
-  {
-    getGpsData();
-    return 8;
+  if (command == "sleep") {
+    // this doesn't work.....
+    System.sleep({}, {}, 60);
+
   }
-  else return -1;
+  if (command == "callgps") {
+    getGpsData(dbLoc);
+    return 8;
+  } else return -1;
 }
